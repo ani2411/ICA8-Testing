952e0a7f572f68480e0d1d8def5edef5002b8b63
ani2411 <103022270+ani2411@users.noreply.github.com>
2022-10-27 22:05:12 -0700

    Add files via upload

---
+++ ICA8-Testing/src/urinals.java
@@ -0,0 +1,50 @@
+// author - Sesha Sai Aneeswar
+
+import java.lang.*;
+import java.util.*;
+
+public class urinals {
+	public static void main(String[] args)
+	{
+		System.out.println("Enter Your Strings: ");
+		getString();
+	}
+	
+	public static void getString()
+	{
+		Scanner sc =new Scanner(System.in);
+		String k = sc.nextLine();
+		while(!k.equals("-1"))
+		{
+			if(validateString(k))
+			{
+				System.out.println("Sucess");
+				
+			}
+			else 
+			{
+				System.out.println("Not Success");
+				
+			}
+			k = sc.nextLine();
+		}
+	}
+	
+	public static boolean validateString(String inpStr)
+	{
+		int c=0;
+		for(int i=0;i<inpStr.length();i++)
+		{
+			if(inpStr.charAt(i)=='1'  || inpStr.charAt(i)== '0')
+				c=0;
+			else
+				c=1;
+		}
+		if(c==0)
+			return true;
+		else
+			return false;
+	}
+
+}
+
